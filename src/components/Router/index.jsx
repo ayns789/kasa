bfff3522bc83581531a8e17585f9f07d4764df6c
github_ayns789 <joly.a.idf@gmail.com>
2023-03-28 12:36:13 +0200

    up paths, router and build for server

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -1,10 +1,49 @@
 import '../../App.css';
 import { BrowserRouter } from 'react-router-dom';
-import Router from './Router.jsx';
+// import Router from './Router.jsx';
 import React, { useEffect, useState } from 'react';
 import { createContext } from 'react';
 import Footer from '../Footer/index.jsx';
 import Navbar from '../Navbar/index.jsx';
+import { Route, Routes } from 'react-router-dom';
+// import routes from './routes.js';
+import About from '../../Pages/about.jsx';
+import Home from '../../Pages/home.jsx';
+import Lodging from '../../Pages/lodging.jsx';
+import Error from '../../Pages/error.jsx';
+
+const routes = [
+  {
+    path: '',
+    element: <Home />,
+    title: 'home',
+  },
+  {
+    path: 'about',
+    element: <About />,
+    title: 'about',
+  },
+  {
+    path: 'lodging/:id',
+    element: <Lodging />,
+    title: 'lodging/:id',
+  },
+  {
+    path: '*',
+    element: <Error />,
+    title: 'error404',
+  },
+];
+
+// export default routes;
+
+const Router = () => {
+  const pageRoutes = routes.map(({ path, title, element }) => {
+    return <Route key={title} path={`/${path}`} element={element} />;
+  });
+
+  return <Routes>{pageRoutes}</Routes>;
+};
 
 const dataLodgings = [];
 export const LodgingContext = createContext(dataLodgings);
@@ -29,3 +68,5 @@ const App = () => {
 };
 
 export default App;
+
+// export default Router;
